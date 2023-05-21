52429691088d3db4ebf22b4da14edb6c77cb6c2a
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-05-21 15:01:53 +1000

    fix(processor): component should add extension if not exist, not raise err

--- juxtorpus/corpus/processors/components/sentiment.py
+++ juxtorpus/corpus/processors/components/sentiment.py
@@ -56,9 +56,8 @@ class Sentiment(object):
 class SentimentComp(Component):
     def __init__(self, nlp: Language, name: str, attr: str):
         super(SentimentComp, self).__init__(nlp, name, attr)
-        if Doc.has_extension(self._attr):
-            raise KeyError(f"{self._attr} already exists. {SentimentComp.__name__} will not function properly.")
-        Doc.set_extension(self._attr, default={})
+        if not Doc.has_extension(self._attr):
+            Doc.set_extension(self._attr, default={})
         self.sentiment = None
 
     def __call__(self, doc: Doc) -> Doc:
