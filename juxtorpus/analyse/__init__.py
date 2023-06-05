86c9f730bc12f5967d160854d29bfa8529e47e1f
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-06-05 16:22:50 +1000

    feat(jux): rm features where dependencies are dropped

--- juxtorpus/analyse/__init__.py
+++ juxtorpus/analyse/__init__.py
@@ -2,13 +2,13 @@ from juxtorpus.corpus import Corpus
 
 # todo: import the analyse functions from each submodule.
 from .topic_model import analyse_with_lda
-from .sentiment import analyse_with_sentiment
-from .quotation import analyse_with_quotation
+# from .sentiment import analyse_with_sentiment
+# from .quotation import analyse_with_quotation
 
 _methods = {
     'topic_model': analyse_with_lda,
-    'sentiment': analyse_with_sentiment,
-    'quotation': analyse_with_quotation
+    # 'sentiment': analyse_with_sentiment,
+    # 'quotation': analyse_with_quotation
 }
 
 _results_prefix = {
