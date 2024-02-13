4da08ae9e4cdccd5954271435f0950ba02f361fa
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2024-02-14 10:20:53 +1100

    feat(stats): integrate atap_corpus

--- juxtorpus/stats/stats.py
+++ juxtorpus/stats/stats.py
@@ -1,8 +1,9 @@
+import weakref
 from typing import TYPE_CHECKING
 
+import numpy as np
 import pandas as pd
 from .loglikelihood_effectsize import log_likelihood_and_effect_size
-from juxtorpus.corpus.freqtable import FreqTable
 from juxtorpus.constants import CORPUS_ID_COL_NAME_FORMAT
 
 if TYPE_CHECKING:
@@ -11,11 +12,24 @@ if TYPE_CHECKING:
 
 class Statistics(object):
     def __init__(self, jux: 'Jux'):
-        self._jux = jux
+        self._jux = weakref.ref(jux)
 
-    def log_likelihood_and_effect_size(self, baseline: FreqTable = None):
-        ftables = [self._jux.corpus_0.dtm.freq_table(nonzero=True),
-                   self._jux.corpus_1.dtm.freq_table(nonzero=True)]
+    def log_likelihood_and_effect_size(self, dtm_names: tuple[str] | str,
+                                       baseline: pd.DataFrame | None = None) -> pd.DataFrame:
+        if isinstance(dtm_names, tuple):
+            d1, d2 = dtm_names
+        elif isinstance(dtm_names, str):
+            d1 = d2 = dtm_names
+        else:
+            raise NotImplementedError(f"dtm_names must be either a tuple of str or str.")
+
+        c0, c1 = self._jux().corpus_0, self._jux().corpus_1
+        dtm0, dtm1 = c0.dtms[d1], c1.dtms[d2]
+        indices: list[np.ndarray] = [np.array(dtm0.terms)[dtm0.terms_vector.nonzero()[0]],
+                                     np.array(dtm1.terms)[dtm1.terms_vector.nonzero()[0]]]
+        values: list[np.ndarray] = [dtm0.terms_vector[dtm0.terms_vector.nonzero()[0]],
+                                    dtm1.terms_vector[dtm1.terms_vector.nonzero()[0]]]
+        ftables = [pd.Series(v, index=index) for v, index in zip(values, indices)]
 
         if baseline is None:
             res = log_likelihood_and_effect_size(ftables)
@@ -31,5 +45,4 @@ class Statistics(object):
                 res.rename(mapper=mapper, axis=1, inplace=True)
                 res_list.append(res)
             res = pd.concat(res_list, axis=1)
-        # reformat to be consistent with jux
         return res
