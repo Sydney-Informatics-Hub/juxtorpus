42b2c013a47e9219d487bbc28f8dd91ed61b1b46
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2024-03-20 14:08:02 +1100

    fix(widgets): corpora widget - download to use base64

--- juxtorpus/viz/widgets/corpus_download.py
+++ juxtorpus/viz/widgets/corpus_download.py
@@ -2,26 +2,28 @@
 
 Provides the widget that allows you to download a corpus.
 """
-from pathlib import Path
+import base64
+import io
+
+import ipywidgets as widgets
 
 from juxtorpus.corpus import Corpus
-import ipywidgets as widgets
-import html
 
 
 def make_download_html_widget_for(corpus: Corpus, ext: str, **kwargs) -> widgets.HTML:
     match ext:
         case '.csv':
-            path = f"{corpus.name}.csv"
-            corpus.to_csv(path)
+            buffer = io.BytesIO()
+            corpus.to_csv(buffer)
+            buffer.seek(0)
         case '.xlsx':
-            path = f"{corpus.name}.xlsx"
-            corpus.to_excel(path)
+            buffer = io.BytesIO()
+            corpus.to_excel(buffer)
+            buffer.seek(0)
         case _:
             raise ValueError("ext must be .csv or .xlsx.")
 
-    path = Path(path)
-    href = html.escape("./" + path.name)
-    default_fname = path.name
-    return widgets.HTML(f'<a href=/files/{href} download={default_fname}>Download</a>', **kwargs)
+    b64encoded: bytes = base64.b64encode(buffer.read())
+    href = f"data:text/csv;base64,{b64encoded.decode()}"
+    return widgets.HTML(f'<a href="{href}" download={corpus.name + ext}>Download<a/>', **kwargs)
     # note: somehow ipywidget's HTML does not direct href to start from /files but IPython's HTML does.
